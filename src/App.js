9fa00c91377c081f33ee7009a2c98847a46d5caf
Metal450 <metal450@gmail.com>
2021-05-05 16:11:59 -0700

    improve repro

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { Link } from 'react-scroll';
 
 function App() {
 
-    const items = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6'];
+    const items = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7'];
     const container = "scrollContainer";
 
     return (
         <main>
 
-            <div className="flex md:flex-row flex-col absolute right-0 bottom-0 left-0 top-0">
-                <div id={container} className="flex xl:flex-row flex-col overflow-auto border border-box max-w-6xl" style={{ flexBasis: "65%", flex: 2}}>
+            <div className="flex sm:flex-row flex-col absolute right-0 bottom-0 left-0 top-0">
+                <div id={container} className="md:flex-row flex flex-col overflow-auto border border-box max-w-6xl" style={{ flexBasis: "65%", flex: 2}}>
 
-                    <div className="p-3 border sticky top-0 flex-shrink-0 bg-white border-box xl:w-64">
+                    <div className="md:w-64 p-3 border sticky top-0 flex-shrink-0 bg-white border-box">
                         {items.map((item) => <div className="cursor-pointer m-2 underline" key={item}><Link to={item} smooth={true} duration={500} spy={true} containerId={container}>{item}</Link></div>)}
                     </div>
 
